89326deda5faefad354e73cf9279d0ab3ccd32e0
tolkienfan2 <minni.ang@gmail.com>
2016-02-29 17:40:22 -0500

    removed scrolling photo

---
+++ Ottawa/AttractionsVC.swift
@@ -0,0 +1,30 @@
+//
+//  AttractionsVC.swift
+//  Ottawa
+//
+//  Created by Minni K Ang on 2016-02-27.
+//  Copyright © 2016 Minni K Ang. All rights reserved.
+//
+
+import UIKit
+
+class AttractionsVC: UIViewController {
+
+    
+    @IBOutlet weak var sightsButton: RoundedButton!
+    
+    @IBOutlet weak var eventsButton: RoundedButton!
+    
+    @IBOutlet weak var winterludeButton: RoundedButton!
+    
+    @IBOutlet weak var birthdayButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func goWinterlude(sender: AnyObject) {
+        performSegueWithIdentifier("goWinterlude", sender: nil)
+    }
+
+}
--- Ottawa/WinterludeVC.swift
+++ Ottawa/WinterludeVC.swift
@@ -10,26 +10,20 @@ import UIKit
 
 class WinterludeVC: UIViewController {
 
+    @IBOutlet weak var backButton: RoundedButton!
+    @IBOutlet weak var winterludeText: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        dispatch_async(dispatch_get_main_queue(), {self.winterludeText.scrollRangeToVisible(NSMakeRange(0, 0))} )
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
-
+    
+    @IBAction func goAttractions(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
