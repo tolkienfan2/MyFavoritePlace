f7bdcca5cfd2c66afd1f72eea7dddc661be1a880
tolkienfan2 <minni.ang@gmail.com>
2016-03-15 10:03:25 -0400

    Added animated buttons

--- Ottawa/RoundedButton.swift
+++ Ottawa/RoundedButton.swift
@@ -14,6 +14,10 @@ class RoundedButton: UIButton {
         
         self.layer.cornerRadius = 5.0
         self.backgroundColor = UIColor(red: 63.0/255.0, green: 128.0/255.0, blue: 203.0/255.0, alpha: 1.0)
+        
+        let delay = 3.0 * drand48()
+        
+        UIView.animateWithDuration(2, delay: delay, usingSpringWithDamping: 0.1, initialSpringVelocity: 0, options: [.AllowUserInteraction, .CurveEaseInOut], animations: {self.transform = CGAffineTransformMakeScale(1.1, 1.1)}, completion: {finish in UIView.animateWithDuration(2) {self.transform = CGAffineTransformIdentity}})
     }
     
     
